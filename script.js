68c2bb831e274fcf579c8305715f93694bbc8d99
Soluciones <anakinjd1985@gmail.com>
2026-06-20 21:57:29 -0600

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const HALLVALLA_BUILD_VERSION="v7HW_det_leader_layout_clean_2026_06_20";
+const HALLVALLA_BUILD_VERSION="v7HW_det_compact_final_2026_06_20";
 import {initializeApp} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {getDatabase,ref,set,update,get,onValue,remove} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 import {getAuth,signInAnonymously,onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
