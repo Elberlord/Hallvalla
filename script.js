0b530f2f3977fb73d2880b5587c9fbd606cbb2b9
Soluciones <anakinjd1985@gmail.com>
2026-08-02 04:51:35 -0600

    update

--- script.js
+++ script.js
@@ -71,15 +71,15 @@ bloques con dependencias delicadas. Eso evita romper inicializadores const/let.
 01_BOOT_CONFIG_IMPORTS
 -------------------------------------------------------------------------------
 */
-const HALLVALLA_BUILD_VERSION="v8_FIELD_CARD_GRID_EDITOR_FINAL_VALUES_7BOARDCTRL8J";
+const HALLVALLA_BUILD_VERSION="v8_FIELD_CARD_GRID_EDITOR_FINAL_VALUES_7BOARDCTRL8K";
 import {initializeApp} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {getDatabase,ref,set,update,get,onValue,remove,runTransaction,serverTimestamp} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 import {getAuth,signInAnonymously,onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 const firebaseConfig={apiKey:"AIzaSyA6C6f3gSVDvgxcQuyD8PsyQiHNDPD_ZOQ",authDomain:"hallvalla-online.firebaseapp.com",projectId:"hallvalla-online",storageBucket:"hallvalla-online.firebasestorage.app",messagingSenderId:"496903032464",appId:"1:496903032464:web:d1e63bfead7109fc905215",databaseURL:"https://hallvalla-online-default-rtdb.firebaseio.com"};
 const app=initializeApp(firebaseConfig),db=getDatabase(app),auth=getAuth(app);
-const FIELD_BOARD_TUNER_KEY="hallvalla_field_board_tuner_v2_final_110_5x8";
+const FIELD_BOARD_TUNER_KEY="hallvalla_field_board_tuner_v3_final_100_5x9";
 const BATTLE_CLOCK_TUNER_KEY="hallvalla_battle_clock_tuner_v2_final_positions";
-const FIELD_BOARD_DEFAULTS=Object.freeze({rows:8,cols:5,cardScale:110});
+const FIELD_BOARD_DEFAULTS=Object.freeze({rows:9,cols:5,cardScale:100});
 const FIELD_BOARD_LIMITS=Object.freeze({rows:[4,12],cols:[3,10],cardScale:[45,150]});
 function clampFieldBoardNumber(value,min,max,fallback){
   const n=Number(value);
