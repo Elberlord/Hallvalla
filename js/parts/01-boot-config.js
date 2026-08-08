f7dab47a57645c8c589815ef3363b86311852bd8
Soluciones <anakinjd1985@gmail.com>
2026-08-07 19:39:44 -0600

    updates

--- js/parts/01-boot-config.js
+++ js/parts/01-boot-config.js
@@ -80,7 +80,7 @@ if(!firebaseConfig?.apiKey||!firebaseConfig?.databaseURL){
 }
 const app=initializeApp(firebaseConfig),db=getDatabase(app),auth=getAuth(app);
 const FIELD_BOARD_TUNER_KEY="hallvalla_field_board_tuner_v3_final_100_5x9";
-const BATTLE_CLOCK_TUNER_KEY="hallvalla_battle_clock_tuner_v2_final_positions";
+const BATTLE_CLOCK_TUNER_KEY="hallvalla_battle_clock_tuner_v3_restored_positions";
 const FIELD_BOARD_DEFAULTS=Object.freeze({rows:9,cols:5,cardScale:100});
 const FIELD_BOARD_LIMITS=Object.freeze({rows:[4,12],cols:[3,10],cardScale:[45,150]});
 function clampFieldBoardNumber(value,min,max,fallback){
